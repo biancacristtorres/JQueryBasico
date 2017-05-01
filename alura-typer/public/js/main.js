89026574c5c607816536d79f4f13b2f4534589a3
biancacristtorres <biancacristtorres34@gmail.com>
2017-05-01 20:30:30 -0300

    segundo capitulo jQuery

--- alura-typer/public/js/main.js
+++ alura-typer/public/js/main.js
@@ -5,3 +5,12 @@ console.log(frase);
 console.log(numPalavras);
 var tamanhoFrase = $(".tamanho-frase").text(numPalavras);
 console.log(tamanhoFrase);
+
+var campo = $(".campo-digitacao");
+campo.on("input", function(){
+      var conteudo = campo.val();
+      var quantidadePalavras = conteudo.split(/\S+/).length - 1;
+      $("#contador-palavras").text(quantidadePalavras);
+      var quantidadeCaracteres = conteudo.length;
+      $("#contador-caracteres").text(quantidadeCaracteres);
+});
