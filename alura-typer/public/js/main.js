212b91a792af71a9bf1abd7cd43b292ff20fe805
biancacristtorres <biancacristtorres34@gmail.com>
2017-05-01 22:26:56 -0300

    terceiro capitulo jQuery

--- alura-typer/public/js/main.js
+++ alura-typer/public/js/main.js
@@ -3,9 +3,11 @@ var frase = $ (".frase").text();
 var numPalavras = frase.split(" ").length;
 console.log(frase);
 console.log(numPalavras);
-var tamanhoFrase = $(".tamanho-frase").text(numPalavras);
+var tamanhoFrase = $("#tamanho-frase").text(numPalavras);
 console.log(tamanhoFrase);
 
+
+//evento de input no campo, porém não reconhece quando o cursor entra no campo através do TAB
 var campo = $(".campo-digitacao");
 campo.on("input", function(){
       var conteudo = campo.val();
@@ -13,4 +15,17 @@ campo.on("input", function(){
       $("#contador-palavras").text(quantidadePalavras);
       var quantidadeCaracteres = conteudo.length;
       $("#contador-caracteres").text(quantidadeCaracteres);
+});
+
+//evento focus, reconhece quando o cursor entra no campo, independente de como ele chegar click, TAB e etc...
+var tempoRestante = $("#tempo-digitacao").text();
+campo.one("focus",function(){//one executa apenas uma única vez, diferente do on que executa toda vez que ele for chamado
+       var cronometroID = setInterval(function(){ // cronometroID é o id do setInterval, todo setInterval possui seu proprio ID, para pega-lo basta receber a função
+        tempoRestante--;
+        $("#tempo-digitacao").text(tempoRestante);
+        if(tempoRestante < 1){
+          campo.attr("disabled",true);
+          clearInterval(cronometroID);
+        }
+      }, 1000);
 });
